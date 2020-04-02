891419c110d94dde33210de45c9767ed8301de11
maticou <maticou123@gmail.com>
2020-04-02 15:44:13 -0300

    mas reportes del director de escuela

--- DB/reportes_director_carrera.sql
+++ DB/reportes_director_carrera.sql
@@ -22,3 +22,81 @@ BEGIN
 	ORDER BY nombre ASC, seccion ASC, anio ASC;
 END;
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION docentes_con_notas_atrasadas(
+	) RETURNS TABLE (
+		docente docente.cedula%TYPE,
+		nombre docente.nombre_completo%TYPE,
+		curso curso.nombre%TYPE
+	) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT docente.cedula AS docente,
+	docente.nombre_completo AS nombre,
+	curso.nombre AS curso
+	FROM evaluacion, instancia_curso, instancia_evaluacion, docente, curso, tipo_evaluacion
+	WHERE evaluacion.fecha <now()::date
+	AND instancia_evaluacion.evaluacion = evaluacion.id
+	AND instancia_curso.id = evaluacion.instancia_curso
+	AND instancia_curso.docente = docente.cedula
+	AND instancia_evaluacion.nota <10
+	GROUP BY docente.nombre_completo, curso.nombre, docente.cedula
+	ORDER BY nombre ASC;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION docentes_con_notas_al_dia(
+	) RETURNS TABLE (
+		docente docente.cedula%TYPE,
+		nombre docente.nombre_completo%TYPE,
+		curso curso.nombre%TYPE
+	) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT docente.cedula AS docente,
+	docente.nombre_completo AS nombre,
+	curso.nombre AS curso
+	FROM evaluacion, instancia_curso, instancia_evaluacion, docente, curso, tipo_evaluacion
+	WHERE evaluacion.fecha <now()::date
+	AND instancia_evaluacion.evaluacion = evaluacion.id
+	AND instancia_curso.id = evaluacion.instancia_curso
+	AND instancia_curso.docente = docente.cedula
+	AND instancia_evaluacion.nota >9
+	GROUP BY docente.nombre_completo, curso.nombre, docente.cedula
+	ORDER BY nombre ASC;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION cursos_con_situacion_alumnos(
+	) RETURNS TABLE (
+		curso curso.nombre%TYPE,
+		periodo tipo_periodo.tipo%TYPE,
+		anio instancia_curso.anio%TYPE,
+		seccion instancia_curso.seccion%TYPE,
+		alumno alumno_instancia_curso.alumno%TYPE,
+		nombre_alumno alumno.nombre_completo%TYPE,
+		situacion situacion_alumno_instancia_curso.situacion%TYPE
+	) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT curso.nombre AS curso,
+	tipo_periodo.tipo AS periodo,
+	instancia_curso.anio AS anio,
+	instancia_curso.seccion AS seccion,
+	alumno_instancia_curso.alumno AS alumno,
+	alumno.nombre_completo AS nombre_alumno,
+	situacion_alumno_instancia_curso.situacion AS situacion
+	FROM alumno_instancia_curso, alumno, curso, instancia_curso, situacion_alumno_instancia_curso, tipo_periodo
+	WHERE curso.id = instancia_curso.curso
+	AND alumno_instancia_curso.instancia_curso = instancia_curso.id
+	AND situacion_alumno_instancia_curso.id = alumno_instancia_curso.situacion
+	AND tipo_periodo.id = instancia_curso.periodo
+	AND alumno_instancia_curso.alumno = alumno.num_matricula
+	ORDER BY curso ASC, anio ASC, seccion ASC;
+END;
+$$ LANGUAGE plpgsql;
